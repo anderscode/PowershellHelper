9f0c021f3f57e418a7eeafb621784b730b4ea97d
Anderscode <git@c-solutions.se>
2016-01-25 16:14:14 +0100

    Timeout not exposed skipping it for now.

--- ITADHelper/startHelper.ps1
+++ ITADHelper/startHelper.ps1
@@ -296,7 +296,6 @@ $ITADHelper_Shown=
 function buttonAbout_Click
 {
 $web = New-Object Net.WebClient
-$web.Timeout = 10 * 1000
 $latestVersion = $web.DownloadString("https://raw.githubusercontent.com/anderscode/PowershellHelper/master/ITADHelper/ver.txt")
 $authorLicense = "The MIT License (MIT)
 
@@ -308,7 +307,7 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
-[void][System.Windows.Forms.MessageBox]::Show("Version:`t$ScriptVersion ($ScriptDate)`nName:`t$authorName`nEmail:`t$authorEmail`nWebSite:`t$authorWebsite`n`n`n$authorLicense","About")
+[void][System.Windows.Forms.MessageBox]::Show("Version:`t$ScriptVersion ($ScriptDate) - Lastest: $latestVersion `nName:`t$authorName`nEmail:`t$authorEmail`nWebSite:`t$authorWebsite`n`n`n$authorLicense","About")
 }
 
 #############################################################
